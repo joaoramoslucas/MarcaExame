6ac623f9414328ee616a7ec8d57255a849905b6a
Joao Lucas <joaopedroramosdelucas10@gmail.com>
2025-02-26 17:48:33 -0300

    First commit

---
+++ Vollmed/Services/WebService.swift
@@ -0,0 +1,53 @@
+//
+//  WebService.swift
+//  Vollmed
+//
+//  Created by Giovanna Moeller on 12/09/23.
+//
+
+import UIKit
+
+struct WebService {
+    
+    private let baseURL = "http://localhost:3000"
+    
+    let imageCache = NSCache<NSString, UIImage>()
+    
+    func donwloadImage(from imageURL: String) async throws -> UIImage? {
+        
+        guard let url = URL(string: imageURL) else {
+            print("Erro ao fazer a requisicao das imagens")
+            return nil
+        }
+        
+        if let cachedImage = imageCache.object(forKey: imageURL as NSString) {
+                print("Imagem carregada do cache")
+                return cachedImage
+        } else {
+            print("Imagem baixada da internet")
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+           guard let image = UIImage(data: data) else {
+               return nil
+           }
+        
+        imageCache.setObject(image, forKey: imageURL as NSString)
+        
+        return image
+    }
+    
+    func getAllSpecialists() async throws -> [Specialist]? {
+        let endpoint = baseURL + "/especialista"
+        
+        guard let url = URL(string: endpoint) else {
+            print("Erro ao criar URL")
+            return nil
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let specialists = try JSONDecoder().decode([Specialist].self, from: data)
+        
+        return specialists
+    }
+}
